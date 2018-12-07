170551246c302b2d4000b48de631abbe3f0c30e4
Jack The Ripper <45645211+jtheripper18@users.noreply.github.com>
2018-12-07 12:48:46 +0800

    Update app.py

--- app.py
+++ app.py
@@ -44,7 +44,7 @@ def plans(bot, update):
     cursor.execute("SELECT * from plans")
     data = cursor.fetchall()
     
-    update.message.reply_text(" " + data[1])
+    update.message.reply_text(" " + data[0])
     
     db.close()
 
