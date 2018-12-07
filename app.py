0b66de1526e792bd4508358cb13dc407b78e6aab
Jack The Ripper <45645211+jtheripper18@users.noreply.github.com>
2018-12-07 14:18:14 +0800

    Update app.py

--- app.py
+++ app.py
@@ -48,7 +48,15 @@ def plans(bot, update):
     strmsg = ""
     
     for row in rows:
-        strmsg += "Plan ID: %s\nPlan: %s\nPlan Location: %s\nMeeting Point: %s\nTransport: %s\nDate / Time: %s\n" % (row[0], row[1], row[2], row[3], row[4], row[5])
+        strmsg += """
+            Plan ID: %s\n
+            Plan: %s\n
+            Plan Location: %s\n
+            Meeting Point: %s\n
+            Transport: %s\n
+            Date / Time: %s\n
+        """ 
+        % (row[0], row[1], row[2], row[3], row[4], row[5])
     
     update.message.reply_text(strmsg)
     
